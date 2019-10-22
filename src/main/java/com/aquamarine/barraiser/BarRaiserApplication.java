8824916d36c6bf577dff7ffcb0aeaded50003120
m_singh <nhpmanpreet@gmail.com>
2019-10-22 11:14:33 -0400

    Init commit

---
+++ src/main/java/com/aquamarine/barraiser/BarRaiserApplication.java
@@ -0,0 +1,13 @@
+package com.aquamarine.barraiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarRaiserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarRaiserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aquamarine/barraiser/BarRaiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquamarine.barraiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarRaiserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
