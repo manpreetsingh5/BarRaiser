a7c11fd3bf031e3c25fb56723168dffb3fac6a56
m_singh <nhpmanpreet@gmail.com>
2019-11-06 19:05:16 -0500

    Added Cohort and Drink models

--- src/main/java/com/aquamarine/barraiser/model/Cohort.java
+++ src/main/java/com/aquamarine/barraiser/model/Cohort.java
@@ -1,25 +1,30 @@
 package com.aquamarine.barraiser.model;
 
 import lombok.*;
+import lombok.experimental.Accessors;
+
 import javax.persistence.*;
 
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//@Entity
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Cohort {
-//    @Id
-//    @GeneratedValue(strategy=GenerationType.AUTO)
-//    private Integer id;
-//
-//    @Column(unique = true, nullable = false)
-//    private String description;
-//
-//    @JoinColumn(name = "instructor_id", referencedColumnName = "id")
-//    private User instructor;
-//
-//    @JoinColumn(name = "user_id", referencedColumnName = "id")
-//    private User user;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String description;
+
+    @OneToOne
+    @JoinColumn(name = "instructor_id", referencedColumnName = "id")
+    private User instructor;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 }
