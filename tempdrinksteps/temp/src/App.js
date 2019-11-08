2096c1172db7a020303b02ff484c6a22db88adf9
Neil Opena <neil1114o@gmail.com>
2019-11-08 14:38:45 -0500

    Added basic progress bar animating

--- tempdrinksteps/temp/src/App.js
+++ tempdrinksteps/temp/src/App.js
@@ -1,52 +1,102 @@
-import React from 'react';
-import ClickNHold from 'react-click-n-hold';
-import './App.css';
+import Repeatable from 'react-repeatable';
+import cx from 'classnames';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container, Row, Card } from 'react-bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+import './App.css';
+import { Button, Container, Row, ProgressBar, Col } from 'react-bootstrap';
 
-
-class DrinkStep extends React.Component {
+class DrinkGame extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      value: 'yeet',
+      progress: 0,
+      target: 72,
+      pressed: false,
+      competed: false,
+      drink_result: null,
     }
   }
 
-  end(e, enough) {
-    console.log('End');
-    if(enough){
-      console.log('Click released after enough time');
-    }else{
-      console.log('Click released too soon');
+  checkProgress() {
+    if (!this.state.completed && this.state.progress > (this.state.target)) {
+      this.setState({
+        completed: true,
+        drink_result: 'FAILED',
+        progress: 0,
+      })
     }
   }
 
-  clickNHold(e) {
-    console.log('Click and Hold');
-  }
 
   render() {
     return (
       <Container>
         <Row>
-          <ClickNHold
-            time={2}
-            onStart={(e) => this.setState({value: 'Start'})}
-            onClickNHold={() => this.clickNHold}
-            onEnd={(e, enough) => 
-              this.setState(enough ? {value: 'Click released after enough time'} : {value: 'Click released too soon'})
-            } >
-            <Card className="bg-card">
-              <Card.Body>
-                <Button>Click and hold</Button>
-              </Card.Body>
-            </Card>
-          </ClickNHold>
+          <Col>
+            <ProgressBar animated now={this.state.progress}>
+            </ProgressBar>
+              <div className="marker" style={{right: (100 - this.state.target) + '%'}}>
+              </div>
+          </Col>
         </Row>
         <Row>
-          <p>{this.state.value}</p>
+          <Col>
+            <h4>Target: {this.state.target}</h4>
+          </Col>
+          <Col>
+            <h4>Current: {this.state.progress}</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Repeatable
+              tag={Button}
+              btnStyle={cx({
+                'default': !this.state.pressed,
+                'info': this.state.pressed,
+              })}
+              repeatDelay={0}
+              repeatInterval={150}
+              onPress={() => {
+              }}
+              onHoldStart={() => {
+              }}
+              onHold={() => {
+                this.setState({
+                  progress: Math.min(this.state.progress + 1, 100)
+                });
+                this.checkProgress();
+              }}
+              onHoldEnd={() => {
+              }}
+              onRelease={() => {
+              }}
+            >
+              Pour Drink
+          </Repeatable>
+          </Col>
+          <Col>
+              <Button variant="success" 
+                onClick={() => {
+                  this.setState({
+                    completed: true,
+                  });
+                  if(this.state.progress === this.state.target){
+                    alert('Congratulations')
+                  }else{
+                    this.setState({
+                      drink_result: 'FAILED',
+                      progress: 0,
+                    })
+                  }
+                }}
+              >Complete</Button>
+          </Col>
+        </Row>
+        <Row>
+          <h5>{this.state.drink_result}</h5>
         </Row>
       </Container>
     );
@@ -58,7 +108,11 @@ class App extends React.Component {
 
   render() {
     return (
-      <DrinkStep />
+      <Container>
+        <Row>
+          <DrinkGame />
+        </Row>
+      </Container>
     );
   }
 }
--- tempdrinksteps/temp/src/index.js
+++ tempdrinksteps/temp/src/index.js
@@ -1,8 +1,10 @@
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
