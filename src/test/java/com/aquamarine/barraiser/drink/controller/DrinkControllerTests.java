e3049028221f0201c36a9d1fb9987c24017f3f7f
Kushal Delhiwala <kushal.delhiwala@stonybrook.edu>
2019-11-18 19:25:20 -0500

    delete cohort controller

--- src/test/java/com/aquamarine/barraiser/drink/controller/DrinkControllerTests.java
+++ src/test/java/com/aquamarine/barraiser/drink/controller/DrinkControllerTests.java
@@ -1,5 +1,13 @@
 package com.aquamarine.barraiser.drink.controller;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EnableJpaAuditing
 public class DrinkControllerTests {
 
 }
