bd1bfbc16bd6e10fde2f29048bfa62b2c2a42432
m_singh <nhpmanpreet@gmail.com>
2019-11-20 18:18:11 -0500

    Fixed getting cohorts and images.

--- src/main/java/com/aquamarine/barraiser/service/cohort/interfaces/CohortService.java
+++ src/main/java/com/aquamarine/barraiser/service/cohort/interfaces/CohortService.java
@@ -6,16 +6,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
 public interface CohortService {
-    int createCohort(CohortDTO cohortdto) throws IOException;
+    int createCohort(CohortDTO cohortdto, MultipartFile multipartFile) throws IOException;
     void addUserToCohort(int cohort_id, int user_id);
     Set<UserDTO> getCohortUsers(int cohort_id);
     Set<CohortDTO> getUserCohorts(int user_id);
-    CohortDTO findById(int id);
+    Map<String, Object> findById(int id) throws IOException;
     UserDTO deleteStudentFromCohort(int cohort_id, int user_id);
     void deleteCohort(CohortDTO cohortDTO);
-    ResponseEntity<byte[]> getCohortPicture(CohortDTO cohortDTO) throws IOException;
+    ResponseEntity<byte[]> getCohortPicture(int cohort_id) throws IOException;
 
 }
