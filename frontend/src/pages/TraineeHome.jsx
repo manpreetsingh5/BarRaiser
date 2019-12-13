a8d68a66520ab128d407864ef860827a83db7bff
Alexander Collado <alexc1117@gmail.com>
2019-12-12 20:47:18 -0500

    Implementing different home pages for professional and trainee

---
+++ frontend/src/pages/TraineeHome.jsx
@@ -0,0 +1,20 @@
+import React, { Fragment, Component } from 'react';
+import style from '../style/Home.module.css';
+
+import Row from 'react-bootstrap/Row'
+
+class TraineeHome extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Row className={style.container}>
+                    <div className={style.titleDiv}>
+                        <h3>Home</h3>
+                    </div>
+                </Row>
+            </Fragment>
+        );
+    }
+}
+
+export default TraineeHome;
