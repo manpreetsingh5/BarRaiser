ff72790096c5a90ad80dd85f46f5028a2973482f
Neil Opena <neil1114o@gmail.com>
2019-12-12 23:50:18 -0500

    Completed home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,19 +2,53 @@ import React, { Fragment, Component } from 'react';
 import style from '../style/Home.module.css';
 
 import Row from 'react-bootstrap/Row'
+import { Container, Card, Col } from 'react-bootstrap';
+import beer_src from '../img/beer-home.png';
 
 class Home extends Component {
     render() {
         return (
             <Fragment>
-                <Row className={style.container}>
+                <Row>
                     <div className={style.titleDiv}>
                         <h3>Home</h3>
                     </div>
                 </Row>
-            </Fragment>
-        );
-    }
-}
 
-export default Home;
+                <Row className="my-4">
+                    <Col md={8} className="mx-auto">
+                        <Card className={style.cardShadow}>
+                            <Card.Body>
+                                <Card.Title className="text-center">Welcome to BarRaiser</Card.Title>
+                                <Card.Text>
+                                    <Row>
+                                        <Col md={4} className="mx-auto">
+                                            <img className="img-fluid" src={beer_src} />
+                                        </Col>
+                                    </Row>
+                                    <hr></hr>
+                                    <p>Select <strong>Drinks</strong> to upload your own drink, ingredient, or equipment. </p>
+                                    <ol>
+                                        <li>Upload <em>any</em> ingredient or equipment using <em>any</em> jpg or png.</li>
+                                        <li>Customize your drink's title, description, and display image.</li>
+                                        <li>Select from our fun yet effective training games to help teach your trainees how to properly make your drink!</li>
+                                    </ol>
+                                    <hr></hr>
+                                    <p>Select <strong>Bars</strong> to create or edit your own bar.</p>
+                                    <ol>
+                                        <li>Customize your bar's title, description, and display image.</li>
+                                        <li>Add trainees directly to your bar using their email address</li>
+                                        <li>Assign your drink to your trainees.</li>
+                                        <li>Keep track of your trainees' drink completion.</li>
+                                    </ol>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Fragment>
+                );
+            }
+        }
+        
+        export default Home;
